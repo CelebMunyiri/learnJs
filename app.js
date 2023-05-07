3fd92f85908a2d28bda3e8e370ec3392b6d6ea72
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-07 20:05:32 +0300

    maps

--- app.js
+++ app.js
@@ -43,7 +43,34 @@ const restaurant = {
     console.log(otherIngredients)
   }
 };
+//MAPS
+const rest=new Map();
+rest.set('name',"classico Italiano");
+rest.set(1,'Firenze','Italy')
+console.log(rest.set(2,'Lisbon','Portugal'))
+rest.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open',11)
+.set('close',23)
+.set(true,'We are open')
+.set(false,'We are closed')
+console.log(rest.get('name'))
+console.log(rest.get(true))
+console.log(rest)
+
+const time=21;
+console.log(rest.get(time>rest.get('open') && time<rest.get('close')))
+console.log(rest.has('categories'));
+
+const arr=[1,2]
+rest.set(arr,'Test')
+console.log(rest.get(arr))
+//console.log(rest.size)
+
+
+
+/*
 //SETS
+
 const orderSet= new Set(['Pasta','Pizza','Risotto','Pasta'])
 console.log(orderSet);
 
@@ -60,7 +87,7 @@ const staff=['Waiter','Chef','Waiter','Manager','Chef','Manager']
 const staffUnique=[...new Set(staff)]
 console.log(staffUnique)
 
-/*
+
 //LOOPING OBJECTS
 const properties=Object.keys(hours)
 //console.log(properties)
