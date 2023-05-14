442830a1053c6e68a23e06757cd2fc0069e3814d
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-14 20:52:09 +0300

    bind method

--- functions.js
+++ functions.js
@@ -89,7 +89,7 @@ document.body.addEventListener('click',high5);
 //abstraction is when we hide implemtation details of a piece of code
 
 
-//FUNCTIONS RETURNING FUNCTIONS*/
+//FUNCTIONS RETURNING FUNCTIONS
 const greet=(greeting)=>{
     return (name)=>{
         console.log(`${greeting} ${name}`)
@@ -107,4 +107,97 @@ const cheki=(infor)=>{
 }
 const nunua=cheki(`Buy`);
 nunua(`mandazi`);
-cheki('shikisha')('Mandunya');
+cheki('shikisha')('Mandunya');*/
+const lufthansa={
+    name:'Lufthansa',
+    iataCode:'LH',
+    bookings:[],
+    book(flightNum,name){
+        console.log(`${name} booked a seat on ${this.name} flight ${this.iataCode} ${flightNum}`)
+    this.bookings.push({flight:`${this.iataCode} ${flightNum}`,name});
+    },
+
+};
+lufthansa.book(239,'Dawud Munyiri');
+lufthansa.book(635,'Abdul Mwangi');
+
+const euroWings={
+    name:'EuroWings',
+    iataCode:'EW',
+    bookings:[],
+
+};
+const book=lufthansa.book;
+//call method
+//book(23,'Bro ndegwa');//does not work
+book.call(euroWings,23,'Samson ndegwa');
+book.call(lufthansa,34,'Joy kemunto');
+
+
+const swiss={
+    name:'Swiss Air Lines',
+    iataCode:'LX',
+    bookings:[],
+}
+book.call(swiss,37,'Lewis James');
+
+console.log(swiss);
+
+//apply method
+const flightData=[583,'John Wlliams']
+book.apply(swiss,flightData)
+console.log(swiss)
+book.call(swiss,...flightData);
+
+//THE BIND METHOD
+//does not immediately call the function but returns a function wth the value we called
+
+//book.call(euroWings,23,'Mahubali the great');
+const bookEw=book.bind(euroWings);
+const bookLh=book.bind(lufthansa);
+const bookKLX=book.bind(swiss);
+
+bookEw(23,'Mahubali the great');
+bookKLX(31,'Abdulraheem')
+bookLh(76,'Nduguu yangu');
+
+const bookEW23=book.bind(euroWings,23,'Muhia')//partial application
+
+//bookEW23('Munyiri Mwangi');
+//bookEW23('Jecinta');
+//bookEW23('munyambu');
+bookEW23();//already have values given
+
+//WITH EVENT LISTENERS
+lufthansa.planes=300;
+lufthansa.buyPlane=function(){
+    console.log(this);
+    this.planes++;
+    console.log(this.planes)
+}
+//lufthansa.buyPlane();
+document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa))
+
+//Partial application
+const addTax=(rate,value)=>
+    value+value*rate;
+console.log(addTax(0.1,200))
+
+const addVAT=addTax.bind(null,0.23);
+//addVAT=value=>value+value*0.23
+
+console.log(addVAT(100));
+console.log(addVAT(300));
+
+//using the grid function
+const addTaxRate=function(rate){
+    return function(value){
+        return value+value*rate
+    }
+}
+
+const men=addTaxRate(0.23)(100);//setting both values as default
+//const addVat=addTaxRate(0.23);//setting one value as default here
+//console.log(addVat(100))//calling method with one default value
+//console.log(addVat(23));//has a default value for rate
+console.log(men);//calling function with both values set as default
